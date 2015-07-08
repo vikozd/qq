d123892bc7afedb8b6ef4df2778ac4d722600146
nana <834518573@qq.com>
2015-07-08 18:07:25 +0800

    qq

--- QQ/src/com/example/qq/MainActivity.java
+++ QQ/src/com/example/qq/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		System.out.println("QQQQ");
+                System.out.println("qqqq");
 	}
 
 	@Override
